6af319d1b75bacab7e3b5fbd849c5a403c2d1255
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-10-19 01:15:01 +0200

    Refactor TodoList component to add a scrollable container for todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,12 +8,14 @@ function TodoList() {
 
     return (
         <div className="rounded-lg flex flex-col gap-2 pt-2 bg-white dark:bg-gray-800">
-            {todos.map(todo => (
-                <div key={todo.id}>
-                    <Todo id={todo.id} title={todo.title} IsCompleted={todo.IsCompleted} />
-                    <hr className="border-gray-300 dark:border-gray-700" />
-                </div>
-            ))}
+            <div className="max-h-96 overflow-auto">
+                {todos.map(todo => (
+                    <div key={todo.id}>
+                        <Todo id={todo.id} title={todo.title} IsCompleted={todo.IsCompleted} />
+                        <hr className="border-gray-300 dark:border-gray-700" />
+                    </div>
+                ))}
+            </div>
             <TodoFooter />
         </div>
     );
